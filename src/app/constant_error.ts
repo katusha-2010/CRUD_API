b9031fc3d10717f3c529c82a5558b3c036ff76b1
katusha-2010 <katusha-2010@mail.ru>
2023-01-12 10:13:59 +0300

    feat: add constant_error.ts

---
+++ src/app/constant_error.ts
@@ -0,0 +1,4 @@
+export const ERR_INVALID_FIELDS = "There are no all necessary fields";
+export const ERR_INVALID_URL = "Invalid url";
+export const ERR_NOT_FOUND_ID = "Such user doesn't exist";
+export const ERR_INVALID_ID = "Invalid id in url";
